9d06271ad082a51d120f3fdf75cf071050a4a498
David Bolic <davidbolic03@gmail.com>
2024-06-04 10:41:06 +0200

    Added Interface and multilayer propreties

---
+++ Connecting database/Collage.Service/StudentService.cs
@@ -0,0 +1,61 @@
+﻿using Collage.Repository;
+using Collage.Repository.Interface;
+using Connecting_database.Models;
+using System.Collections.Generic;
+
+namespace Collage.Service
+{
+    public class StudentService
+    {
+        private readonly StudentRepository _studentRepository;
+
+        public StudentService(StudentRepository studentRepository)
+        {
+            _studentRepository = studentRepository;
+        }
+
+        public int CreateStudent(Student student, int[] majorIds)
+        {
+            int studentId = _studentRepository.CreateStudent(student);
+
+            foreach (var majorId in majorIds)
+            {
+                var studentMajor = new StudentMajor
+                {
+                    StudentId = studentId,
+                    MajorId = majorId
+                };
+
+                // Add logic to insert into StudentMajor table
+            }
+
+            return studentId;
+        }
+
+        public Student GetStudentById(int studentId)
+        {
+            return _studentRepository.GetStudentById(studentId);
+        }
+
+        public void UpdateStudent(Student student, int[] majorIds)
+        {
+            _studentRepository.UpdateStudent(student);
+
+            foreach (var majorId in majorIds)
+            {
+                var studentMajor = new StudentMajor
+                {
+                    StudentId = student.Id,
+                    MajorId = majorId
+                };
+
+                // Add logic to update StudentMajor table
+            }
+        }
+
+        public void DeleteStudent(int studentId)
+        {
+            _studentRepository.DeleteStudent(studentId);
+        }
+    }
+}
---
+++ Connecting database/Repository/Interface/IStudentRepository.cs
@@ -0,0 +1,17 @@
+﻿using Connecting_database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collage.Repository.Interface
+{
+    internal interface IStudentRepository
+    {
+        int CreateStudent(Student student);
+        Student GetStudentById(int studentId);
+        void UpdateStudent(Student student);
+        void DeleteStudent(int studentId);
+    }
+}
