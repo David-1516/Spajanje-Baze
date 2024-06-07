a2cb50bea41f79b1898ccea46aa0be1217eea48f
David Bolic <davidbolic03@gmail.com>
2024-06-07 12:22:18 +0200

    added new connection string DI

--- Connecting database/Connecting database/Program.cs
+++ Connecting database/Connecting database/Program.cs
@@ -11,19 +11,30 @@ using AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 builder.Services.AddControllers();
-builder.Configuration.AddJsonFile("appsettings.json");
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
 
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
 builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
 {
-    containerBuilder.RegisterType<StudentRepository>().As<IStudentRepository>().InstancePerLifetimeScope();
+
+    containerBuilder.RegisterInstance(connectionString).Named<string>("DefaultConnection");
+
+
+    containerBuilder.RegisterType<StudentRepository>()
+        .As<IStudentRepository>()
+        .WithParameter(new NamedParameter("connectionString", connectionString))
+        .InstancePerLifetimeScope();
+
     containerBuilder.RegisterType<StudentService>().As<IStudentService>().InstancePerLifetimeScope();
 });
 
